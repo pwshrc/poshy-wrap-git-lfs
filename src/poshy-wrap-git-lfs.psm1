1102d2ab1973133eaceac1fa71a8f6c3097dfb95
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-09-27 18:50:01 -0400

    ♻ Makes Module Exports Explicit

--- src/poshy-wrap-git-lfs.psm1
+++ src/poshy-wrap-git-lfs.psm1
@@ -18,26 +18,28 @@ function Invoke-GitLfsInstall {
     & $git_bin lfs install @args
 }
 Set-Alias -Name glfsi -Value Invoke-GitLfsInstall
+Export-ModuleMember -Function Invoke-GitLfsInstall -Alias glfsi
 
 function Add-GitLfsTrackedFiles() {
     & $git_bin lfs track @args
 }
 Set-Alias -Name glfst -Value Add-GitLfsTrackedFiles
+Export-ModuleMember -Function Add-GitLfsTrackedFiles -Alias glfst
 
 function Get-GitLfsTrackedFiles() {
     & $git_bin lfs ls-files @args
 }
 Set-Alias -Name glfsls -Value Get-GitLfsTrackedFiles
+Export-ModuleMember -Function Get-GitLfsTrackedFiles -Alias glfsls
 
 function Invoke-GitLfsMigrateImport() {
     & $git_bin lfs migrate import --include= @args
 }
 Set-Alias -Name glfsmi -Value Invoke-GitLfsMigrateImport
+Export-ModuleMember -Function Invoke-GitLfsMigrateImport -Alias glfsmi
 
 function gplfs() {
     [string] $b = "$(git_current_branch)"
     & $git_bin lfs push origin "$b" --all
 }
-
-
-Export-ModuleMember -Function * -Alias *
+Export-ModuleMember -Function gplfs
